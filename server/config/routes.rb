3419ba4c2d490480377a79348b51f0663fbccc11
Elben Shira <elbenshira@gmail.com>
2010-10-15 20:31:27 -0500

    scaffolding lots of models

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,10 @@
 Codewithus::Application.routes.draw do
+  resources :commits
+
+  resources :events
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
